de5843d2863ae31bf90db17f3e99cd1d9e1e351c
sarthakrana21 <sarthak.rana0321@gmail.com>
2024-11-28 17:10:30 +0530

    project setup

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const HomePage = () => {
+  return (
+    <div>
+      <div className='text-5xl text-red-500'>HomePage</div>
+    </div>
+  )
+}
+
+export default HomePage
