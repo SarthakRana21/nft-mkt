277db3546e7f9e8304ccf24d26d2fefa20d46756
sarthakrana21 <sarthak.rana0321@gmail.com>
2024-12-05 15:04:55 +0530

    Hotbids section completed

--- components/Home/Hero.tsx
+++ components/Home/Hero.tsx
@@ -12,8 +12,8 @@ function Hero () {
         </div>
         {/* text content */}
         <div className="col-span-2">
-          <h1 className='text-2xl sm:text-4xl lg:text-5xl xl:text-6xl text-blue-900 font-bold text-left lg:text-center'>Buy, Sell and Collect NFTs</h1>
-          <p className="text-sm md:text-[17px] text-gray-700 text-opacity-80 mt-4 font-medium text-left lg:text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam impedit incidunt possimus suscipit ad iste, fugit culpa voluptatum sed, earum est, iusto molestiae qui vel ut rerum quos! Unde, cum?</p>
+          <h1 className='text-2xl sm:text-4xl lg:text-5xl xl:text-6xl text-blue-900 font-bold text-center lg:text-left'>Buy, Sell and Collect NFTs</h1>
+          <p className="text-sm md:text-[17px] text-gray-700 text-opacity-80 mt-4 font-medium text-center lg:text-left">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam impedit incidunt possimus suscipit ad iste, fugit culpa voluptatum sed, earum est, iusto molestiae qui vel ut rerum quos! Unde, cum?</p>
           <div className="flex items-center justify-center lg:justify-start space-x-3 mt-10">
             <button className="w-36 rounded-full py-3 px-8 text-center bg-blue-900 font-semibold text-white transition-all hover:bg-blue-950">Upload</button>
             <button className="w-36 rounded-full py-3 px-8 text-center bg-rose-900 font-semibold text-white transition-all hover:bg-rose-950">Explore</button>
--- components/Home/Hotbids/HotbidCard.tsx
+++ components/Home/Hotbids/HotbidCard.tsx
@@ -3,6 +3,7 @@
 import React from 'react'
 import Tilt from 'react-parallax-tilt'
 import Image from 'next/image'
+import { BsHeart } from 'react-icons/bs'
 
 type Props = {
     data: {
@@ -25,7 +26,19 @@ const HotbidCard = ({data}:Props) => {
             <div className="">
                 <div className="flex mt-6 items-center justify-between">
                     <h1 className="text-blue-950 hover:text-blue-700 transition-all duration-200 text-base font-bold">{data.name}</h1>
-                    <div className="rounded-lg border-2 border-gray-300 border-opacity-30 px-3 text-sm text-green-800 text-semibold">{data.eth}eth</div>
+                    <div className="rounded-lg border-2 border-gray-400 border-opacity-70 px-2 py-1 text-xs text-green-800 font-bold">{data.eth} ETH</div>
+                </div>
+                <div className="w-full my-2">
+                    <p className="text-sm text-gray-600">Current Bids <span className="text-yellow-600">{data.currentBid}</span> ETH</p>
+                </div>
+                <div className="flex mt-3 items-center justify-between">
+                    <div className="text-blue-950 text-base font-bold text-sm">
+                        Place Bid
+                    </div>
+                    <div className="flex items-center justify-center gap-1 text-red-500">
+                        <BsHeart />
+                        {data.love}
+                    </div>
                 </div>
             </div>
         </div>
